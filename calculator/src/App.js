4cbb0bc41ce87d749aa6f2f3d7101e1fa13aa6ee
Cocoa243 <64682308+Cocoa243@users.noreply.github.com>
2021-08-26 22:43:09 +0200

    .

---
+++ calculator/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Calculator from './Calculator/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+               <p>
+       Calculator made with React JS
+        </p>
+        <Calculator />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
